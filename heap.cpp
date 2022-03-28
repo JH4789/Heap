69305fee306112eed833196f2b73300801d98773
Jayden Huang <411914@beaverton.k12.or.us>
2022-03-28 14:28:43 -0700

    PrintWorking

--- heap.cpp
+++ heap.cpp
@@ -1,26 +1,44 @@
 #include <iostream>
 #include "Node.h"
-
-
+#include <cstring>
+/*
+FOR DELETE FUNCTION:
+Swap the root node and the bottom right node
+Add the root node to the output
+Recursively check if the swapped bottom right node is the smalest thing
+Iterate to the bottom of the tree
+Rinse and Repeat
+*/
 using namespace std;
 void addHeap(Node* newnode, Node* & head, Node* array[], int & sizecount);
+void printFormat(Node* head, int space);
 void postfixprint(Node* current);
 int main() {
   Node* heaparray[100];
   int sizecount = 1;
   Node* treehead = NULL;
-  int input;
+  int input = 0;
   bool running = true;
+  char commandinput[7];
   while(running == true){
-    cout << "Insert number" << endl;
-    cin >> input;
-    if(input == 0) {
-      break;
+    cout << "Please enter your input" << endl;
+    cin >> commandinput;
+    if(strcmp(commandinput, "ADD") == 0) {
+       cout << "Insert number" << endl;
+       cin >> input;
+       Node* newnode = new Node(input);
+       addHeap(newnode, treehead, heaparray, sizecount);
     }
-    Node* newnode = new Node(input);
-    addHeap(newnode, treehead, heaparray, sizecount);
+    else if (strcmp(commandinput, "PRINT") == 0) {
+        printFormat(treehead, 0);
+    }
+    else if (strcmp(commandinput, "QUIT") == 0) {
+      running = false;
+    }
+    else {
+      cout << "Please enter a valid input";
+    }
   }
-  postfixprint(treehead);
   return 0;
 }
 void addHeap(Node* newnode, Node* & head, Node* array[], int & sizecount) {
@@ -44,26 +62,44 @@ void addHeap(Node* newnode, Node* & head, Node* array[], int & sizecount) {
       sizecount++;
     }
     
-    while(array[parentindex-1]->getData() < current->getData()) {
-      
+    while(array[parentindex-1]->getData() < current->getData()) {   
       int replacedata = current->getData();
       current->setData(array[parentindex-1]->getData());
       array[parentindex-1]->setData(replacedata);
+      current = array[parentindex-1];
       if(parentindex == 1) {
-        break;
+        //cout << "uhoh";
+	break;
       }
       else {
-      parentindex = (int)parentindex / 2;
+        parentindex = (int)parentindex / 2;
       }
     }
   }
-  /*
-  else if() {
-
-  }
-  */
 
 }
+void printFormat(Node* head, int space) {
+  if (head == NULL) {
+        return;
+  }
+    // Increase distance between levels
+    space = space + 3;
+ 
+    // Process right child first
+    printFormat(head->getRight(), space);
+ 
+    // Print current node after space
+    // count
+    cout<<endl;
+    for (int i = 0; i < space; i++) {
+        cout<<" ";
+    }
+    cout<<head->getData()<<"\n";
+ 
+    // Process left child
+    printFormat(head->getLeft(), space);
+}
+
 void postfixprint(Node* current) {
   if(current == NULL) {
     return;
